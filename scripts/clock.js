6a11e4cbe4f470003dc02dc8af7b09acc30ae7dd
kn0wht1ms4yn <knowhtimsayn@gmail.com>
2025-03-20 20:42:34 -0400

    fixed clock

--- scripts/clock.js
+++ scripts/clock.js
@@ -2,11 +2,14 @@ function formatDigits(i) {
     return i.toString().padStart(2, '0');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function setClock() {
     clockEl = document.getElementById('clockTime');
-    setInterval(() => {
-        const now = new Date();
-        clockEl.textContent = `${formatDigits(now.getHours())}:${formatDigits(now.getMinutes())}:${formatDigits(now.getSeconds())}`;
-    }, 1000);
+    const now = new Date();
+    clockEl.textContent = `${formatDigits(now.getHours())}:${formatDigits(now.getMinutes())}:${formatDigits(now.getSeconds())}`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setClock();
+    setInterval(setClock, 1000);
     
 });
