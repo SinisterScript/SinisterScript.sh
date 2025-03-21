22cd289bbc8eba42be67083cb88b90aea399ca4e
cede555 <cede555@protonmail.com>
2025-03-21 15:03:42 +0000

    Removed seconds, cleaned up system tray, added volume icon

--- scripts/clock.js
+++ scripts/clock.js
@@ -5,7 +5,7 @@ function formatDigits(i) {
 function setClock() {
     clockEl = document.getElementById('clockTime');
     const now = new Date();
-    clockEl.textContent = `${formatDigits(now.getHours())}:${formatDigits(now.getMinutes())}:${formatDigits(now.getSeconds())}`;
+    clockEl.textContent = `${formatDigits(now.getHours())}:${formatDigits(now.getMinutes())}`; // ${formatDigits(now.getSeconds())} Win 98 didn't have seconds, commenting out.
 }
 
 document.addEventListener('DOMContentLoaded', () => {
